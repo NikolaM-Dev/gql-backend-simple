e4b83b0f4f2858e70447e8c9ebf6a7ba69a6c865
David Merchan <juan1014166@gmail.com>
2021-10-18 16:29:23 -0500

    feat: Add context

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ const { ApolloServer } = require('apollo-server');
 const { resolvers } = require('./schema/resolvers.js');
 const { typeDefs } = require('./schema/type-defs.js');
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => req,
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
