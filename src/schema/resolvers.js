b4572b04bcfb8c2f15f9768b4bb9c21e00a17a87
David Merchan <juan1014166@gmail.com>
2021-10-19 15:46:23 -0500

    feat: Add movies resolvers & get favorite movie by user

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -1,12 +1,24 @@
 const _ = require('lodash');
 
-const { UserList } = require('../FakeData');
+const { MovieList, UserList } = require('../FakeData');
 
 const resolvers = {
   Query: {
     // USERS RESOLVERS
     users: () => UserList,
     user: (_parent, { id }) => _.find(UserList, { id: Number(id) }),
+
+    // MOVIES RESOLVERS
+    movies: () => MovieList,
+    movie: (_parent, { name }) => _.find(MovieList, { name }),
+  },
+  User: {
+    favoriteMovies: () =>
+      _.filter(
+        MovieList,
+        (movie) =>
+          movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010,
+      ),
   },
 };
 
