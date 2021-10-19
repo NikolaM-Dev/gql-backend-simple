8ba495998f761afd6bc284d907dca48ba20b5ee5
David Merchan <juan1014166@gmail.com>
2021-10-19 15:25:59 -0500

    feat: Add querys with args and enum type

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -1,8 +1,12 @@
+const _ = require('lodash');
+
 const { UserList } = require('../FakeData');
 
 const resolvers = {
   Query: {
+    // USERS RESOLVERS
     users: () => UserList,
+    user: (_parent, { id }) => _.find(UserList, { id: Number(id) }),
   },
 };
 
